b763b8155feffb8b1b54a95b5e4ee9fe5acda523
Ruslan Mavletkulov <rusaym@gmail.com>
2025-10-03 23:06:28 +0300

    Database setup

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
@@ -16,12 +16,8 @@ const NotFoundPage = () => {
       <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find requested page</p>
-        <Button
-          variant="outline"
-          className="mt-4 ml2"
-          onClick={() => (window.location.href = "/")}
-        >
-          Back To Home
+        <Button variant="outline" className="mt-4 ml2" asChild>
+          <Link href="/">Back To Home</Link>
         </Button>
       </div>
     </div>
